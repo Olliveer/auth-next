21bb412b32018423c98816aa8560ea4df0ce58c6
olliveer <j.oliveira90@live.com>
2024-01-21 20:33:11 -0300

    feat: add login form with validation and error handling

---
+++ src/schemas/index.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const LoginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(1, {
+    message: "Password must be at least 6 characters long",
+  }),
+});
+
+export const RegisterSchema = z.object({
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters long" }),
+  name: z.string(),
+});
